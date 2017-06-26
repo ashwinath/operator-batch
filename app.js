7f2529460d63f7d28dd9c9355e094b120f1fcf0c
Ashwin Chatterji <ashwinath@hotmail.com>
2017-06-26 22:26:14 +0800

    changed formatting

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ logger.info("Operator Batch has started.");
  * Weather Job
  */
 schedule.scheduleJob('*/5 * * * *', () => {
-  logger.info("Job for <Weather> started.");
+  logger.info("Job for Weather started.");
   downloadWeather();
 });
 
@@ -18,26 +18,26 @@ schedule.scheduleJob('*/5 * * * *', () => {
  * News Jobs
  */
 schedule.scheduleJob('*/30 * * * *', () => {
-  logger.info(`Job for <News:${NewsContract.BBC}> started.`);
+  logger.info(`Job for News:${NewsContract.BBC} started.`);
   downloadNews(NewsContract.BBC);
 });
 
 schedule.scheduleJob('*/30 * * * *', () => {
-  logger.info(`Job for <News:${NewsContract.TECHCRUNCH}> started.`);
-  downloadNews(NewsContract.BBC);
+  logger.info(`Job for News:${NewsContract.TECHCRUNCH} started.`);
+  downloadNews(NewsContract.TECHCRUNCH);
 });
 
 schedule.scheduleJob('*/30 * * * *', () => {
-  logger.info(`Job for <News:${NewsContract.BLOOMBERG}> started.`);
-  downloadNews(NewsContract.BBC);
+  logger.info(`Job for News:${NewsContract.BLOOMBERG} started.`);
+  downloadNews(NewsContract.BLOOMBERG);
 });
 
 schedule.scheduleJob('*/30 * * * *', () => {
-  logger.info(`Job for <News:${NewsContract.BUSINESS_INSIDER}> started.`);
-  downloadNews(NewsContract.BBC);
+  logger.info(`Job for News:${NewsContract.BUSINESS_INSIDER} started.`);
+  downloadNews(NewsContract.BUSINESS_INSIDER);
 });
 
 schedule.scheduleJob('*/30 * * * *', () => {
-  logger.info(`Job for <News:${NewsContract.REUTERS}> started.`);
-  downloadNews(NewsContract.BBC);
+  logger.info(`Job for News:${NewsContract.REUTERS} started.`);
+  downloadNews(NewsContract.REUTERS);
 });
